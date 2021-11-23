df2935714b8df8d22b90e2c5c3b2fb73394a74e6
Linas Nikiperavičius <linas@linasdev.com>
2021-11-23 03:47:33 +0200

    Run rustfmt

--- src/parser.rs
+++ src/parser.rs
@@ -1,23 +1,26 @@
-use std::slice::Iter;
 use std::collections::BTreeMap;
 use std::convert::TryInto;
+use std::slice::Iter;
 
-use ross_config::Value;
 use ross_config::config::Config;
 use ross_config::event_processor::EventProcessor;
-use ross_config::matcher::Matcher;
 use ross_config::extractor::*;
-use ross_config::filter::*;
 use ross_config::filter::state_filter::*;
-use ross_config::producer::*;
+use ross_config::filter::*;
+use ross_config::matcher::Matcher;
 use ross_config::producer::state_producer::*;
+use ross_config::producer::*;
+use ross_config::Value;
 use ross_protocol::event::event_code::*;
 
-use crate::tokenizer::{Token, KeywordToken, SymbolToken, DataToken, Tokenizer, TokenizerError};
+use crate::tokenizer::{DataToken, KeywordToken, SymbolToken, Token, Tokenizer, TokenizerError};
 
 macro_rules! prepare_variable {
     ($variable_map:expr, $variable_name:tt) => {
-        $variable_map.insert(String::from(stringify!($variable_name)), Variable::Integer($variable_name.into()));
+        $variable_map.insert(
+            String::from(stringify!($variable_name)),
+            Variable::Integer($variable_name.into()),
+        );
     };
 }
 
@@ -34,7 +37,7 @@ macro_rules! match_text_token {
 macro_rules! match_symbol_token {
     ($token_iterator:expr, $symbol_token:path) => {
         match $token_iterator.next() {
-            Some(Token::Symbol($symbol_token)) => {},
+            Some(Token::Symbol($symbol_token)) => {}
             Some(token) => return Err(ParserError::UnexpectedToken(token.clone())),
             None => return Err(ParserError::UnexpectedEndOfFile),
         };
@@ -70,13 +73,13 @@ impl<'a> Parser {
     pub fn parse(text: &str) -> Result<Config, ParserError> {
         let mut initial_state = BTreeMap::new();
         let mut variable_map = BTreeMap::new();
-        let mut event_processors = vec!();
+        let mut event_processors = vec![];
 
         Self::prepare_variable_map(&mut variable_map);
 
         let tokens = Tokenizer::tokenize(text)?;
         let mut token_iterator = tokens.iter();
-        
+
         while let Some(token) = token_iterator.next() {
             match token {
                 Token::Keyword(KeywordToken::Let) => {
@@ -84,20 +87,23 @@ impl<'a> Parser {
                     let state = Self::parse_let_statement(&mut token_iterator, &mut state_name)?;
 
                     let mut state_index = 0;
-                    
+
                     while let Some(_) = initial_state.get(&state_index) {
                         state_index += 1;
                     }
 
                     initial_state.insert(state_index, state);
-                    if let Some(_) = variable_map.insert(state_name, Variable::Integer(state_index.into())) {
+                    if let Some(_) =
+                        variable_map.insert(state_name, Variable::Integer(state_index.into()))
+                    {
                         return Err(ParserError::DuplicateVariable);
                     }
-                },
+                }
                 Token::Keyword(KeywordToken::Do) => {
-                    let event_processor = Self::parse_do_statement(&mut token_iterator, &variable_map)?;
+                    let event_processor =
+                        Self::parse_do_statement(&mut token_iterator, &variable_map)?;
                     event_processors.push(event_processor);
-                },
+                }
                 _ => return Err(ParserError::UnexpectedToken(token.clone())),
             }
         }
@@ -108,7 +114,10 @@ impl<'a> Parser {
         })
     }
 
-    fn parse_let_statement(token_iterator: &mut Iter<Token>, state_name: &mut String) -> Result<Value, ParserError> {
+    fn parse_let_statement(
+        token_iterator: &mut Iter<Token>,
+        state_name: &mut String,
+    ) -> Result<Value, ParserError> {
         *state_name = match_text_token!(token_iterator);
 
         match_symbol_token!(token_iterator, SymbolToken::Colon);
@@ -136,10 +145,13 @@ impl<'a> Parser {
         Ok(state)
     }
 
-    fn parse_do_statement(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<EventProcessor, ParserError> {
+    fn parse_do_statement(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<EventProcessor, ParserError> {
         match_symbol_token!(token_iterator, SymbolToken::OpenBrace);
 
-        let mut matchers = vec!();
+        let mut matchers = vec![];
         let mut extractor: Box<dyn Extractor> = Box::new(NoneExtractor::new());
         let mut producer: Box<dyn Producer> = Box::new(NoneProducer::new());
 
@@ -148,12 +160,12 @@ impl<'a> Parser {
                 Token::Keyword(KeywordToken::Match) => {
                     let matcher = Self::parse_match_statement(token_iterator, variable_map)?;
                     matchers.push(matcher);
-                },
+                }
                 Token::Keyword(KeywordToken::Fire) => {
                     let result = Self::parse_fire_statement(token_iterator, variable_map)?;
                     extractor = result.0;
                     producer = result.1;
-                },
+                }
                 Token::Symbol(SymbolToken::CloseBrace) => break,
                 _ => return Err(ParserError::UnexpectedToken(token.clone())),
             }
@@ -166,7 +178,10 @@ impl<'a> Parser {
         })
     }
 
-    fn parse_match_statement(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<Matcher, ParserError> {
+    fn parse_match_statement(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<Matcher, ParserError> {
         match_symbol_token!(token_iterator, SymbolToken::OpenBrace);
 
         let mut sub_token_iterator = token_iterator.clone();
@@ -196,13 +211,13 @@ impl<'a> Parser {
 
         let filter = Self::parse_filter(&mut sub_token_iterator, variable_map)?;
 
-        Ok(Matcher {
-            extractor,
-            filter,
-        })
+        Ok(Matcher { extractor, filter })
     }
 
-    fn parse_fire_statement(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<(Box<dyn Extractor>, Box<dyn Producer>), ParserError> {
+    fn parse_fire_statement(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<(Box<dyn Extractor>, Box<dyn Producer>), ParserError> {
         match_symbol_token!(token_iterator, SymbolToken::OpenBrace);
 
         let mut sub_token_iterator = token_iterator.clone();
@@ -235,7 +250,10 @@ impl<'a> Parser {
         Ok((extractor, producer))
     }
 
-    fn parse_extractor(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<Box<dyn Extractor>, ParserError> {
+    fn parse_extractor(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<Box<dyn Extractor>, ParserError> {
         let extractor_type = match_text_token!(token_iterator);
 
         let arguments = Self::parse_arguments(token_iterator, variable_map)?;
@@ -261,7 +279,10 @@ impl<'a> Parser {
         Err(ParserError::UnknownExtractor(extractor_type))
     }
 
-    fn parse_filter(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<Box<dyn Filter>, ParserError> {
+    fn parse_filter(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<Box<dyn Filter>, ParserError> {
         let filter_type = match_text_token!(token_iterator);
 
         let arguments = Self::parse_arguments(token_iterator, variable_map)?;
@@ -274,7 +295,9 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(U8IncrementStateFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -284,7 +307,9 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(U16IsEqualFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -294,8 +319,12 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(U32IsEqualStateFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[1].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[1]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -305,7 +334,9 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(U32IncrementStateFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -315,8 +346,12 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(U32SetStateFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[1].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[1]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -326,7 +361,9 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(FlipFlopFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -336,8 +373,12 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(CountFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[1].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[1]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -347,15 +388,22 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(CountStateFilter::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[1].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[1]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
         Err(ParserError::UnknownFilter(filter_type))
     }
 
-    fn parse_producer(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<Box<dyn Producer>, ParserError> {
+    fn parse_producer(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<Box<dyn Producer>, ParserError> {
         let producer_type = match_text_token!(token_iterator);
 
         let arguments = Self::parse_arguments(token_iterator, variable_map)?;
@@ -376,9 +424,15 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(BcmChangeBrightnessProducer::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[1].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[2].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[1]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[2]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
@@ -388,25 +442,34 @@ impl<'a> Parser {
             }
 
             return Ok(Box::new(BcmChangeBrightnessStateProducer::new(
-                arguments[0].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[1].try_into().map_err(|_| ParserError::DataError)?,
-                arguments[2].try_into().map_err(|_| ParserError::DataError)?,
+                arguments[0]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[1]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
+                arguments[2]
+                    .try_into()
+                    .map_err(|_| ParserError::DataError)?,
             )));
         }
 
         Err(ParserError::UnknownProducer(producer_type))
     }
 
-    fn parse_arguments(token_iterator: &mut Iter<Token>, variable_map: &BTreeMap<String, Variable>) -> Result<Vec<Variable>, ParserError> {
+    fn parse_arguments(
+        token_iterator: &mut Iter<Token>,
+        variable_map: &BTreeMap<String, Variable>,
+    ) -> Result<Vec<Variable>, ParserError> {
         match_symbol_token!(token_iterator, SymbolToken::OpenParenthesis);
 
-        let mut arguments = vec!();
+        let mut arguments = vec![];
         let mut comma_next = false;
 
         while let Some(token) = token_iterator.next() {
             if comma_next {
                 match token {
-                    Token::Symbol(SymbolToken::Comma) => {},
+                    Token::Symbol(SymbolToken::Comma) => {}
                     Token::Symbol(SymbolToken::CloseParenthesis) => break,
                     _ => return Err(ParserError::UnexpectedToken(token.clone())),
                 }
@@ -416,17 +479,17 @@ impl<'a> Parser {
                 match token {
                     Token::Data(DataToken::Integer(value)) => {
                         arguments.push(Variable::Integer(*value));
-                    },
+                    }
                     Token::Data(DataToken::Boolean(value)) => {
                         arguments.push(Variable::Boolean(*value));
-                    },
+                    }
                     Token::Text(value) => {
                         if let Some(variable) = variable_map.get(value) {
                             arguments.push(*variable);
                         } else {
                             return Err(ParserError::UndefinedVariable(value.clone()));
                         }
-                    },
+                    }
                     Token::Symbol(SymbolToken::CloseParenthesis) => break,
                     _ => return Err(ParserError::UnexpectedToken(token.clone())),
                 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -38,14 +38,14 @@ pub struct Tokenizer {}
 
 #[derive(Debug)]
 pub enum TokenizerError {
-    LexerError(regex::Error)
+    LexerError(regex::Error),
 }
 
 impl Tokenizer {
     pub fn tokenize(text: &str) -> Result<Vec<Token>, TokenizerError> {
         let lexer = Self::build_lexer()?;
         let mut token_iterator = lexer.tokens(text);
-        let mut tokens = vec!();
+        let mut tokens = vec![];
 
         while let Some(token) = token_iterator.next() {
             tokens.push(token);
@@ -58,7 +58,9 @@ impl Tokenizer {
         Ok(LexerBuilder::new()
             .token(r"\s+", |_| None)
             .token(r"//.*\n", |_| None)
-            .token(r"(_|[a-zA-Z])[a-zA-Z_0-9]*", |token| Some(Token::Text(String::from(token))))
+            .token(r"(_|[a-zA-Z])[a-zA-Z_0-9]*", |token| {
+                Some(Token::Text(String::from(token)))
+            })
             .token(r"let", |_| Some(Token::Keyword(KeywordToken::Let)))
             .token(r"do", |_| Some(Token::Keyword(KeywordToken::Do)))
             .token(r"match", |_| Some(Token::Keyword(KeywordToken::Match)))
@@ -67,12 +69,18 @@ impl Tokenizer {
             .token(r":", |_| Some(Token::Symbol(SymbolToken::Colon)))
             .token(r",", |_| Some(Token::Symbol(SymbolToken::Comma)))
             .token(r"\(", |_| Some(Token::Symbol(SymbolToken::OpenParenthesis)))
-            .token(r"\)", |_| Some(Token::Symbol(SymbolToken::CloseParenthesis)))
+            .token(r"\)", |_| {
+                Some(Token::Symbol(SymbolToken::CloseParenthesis))
+            })
             .token(r"\{", |_| Some(Token::Symbol(SymbolToken::OpenBrace)))
             .token(r"\}", |_| Some(Token::Symbol(SymbolToken::CloseBrace)))
             .token(r"=", |_| Some(Token::Symbol(SymbolToken::EqualSign)))
-            .token(r"-?[0-9]+", |token| Some(Token::Data(DataToken::Integer(token.parse().unwrap()))))
-            .token(r"(true|false)", |token| Some(Token::Data(DataToken::Boolean(token.parse().unwrap()))))
+            .token(r"-?[0-9]+", |token| {
+                Some(Token::Data(DataToken::Integer(token.parse().unwrap())))
+            })
+            .token(r"(true|false)", |token| {
+                Some(Token::Data(DataToken::Boolean(token.parse().unwrap())))
+            })
             .build()?)
     }
 }
