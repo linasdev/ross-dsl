351942e887dd00631fb3fa714818dd197cf01c38
Linas Nikiperavičius <linas@linasdev.com>
2021-11-25 23:03:11 +0200

    Add another example

---
+++ examples/send_event.rs
@@ -0,0 +1,12 @@
+use ross_dsl::Parser;
+
+fn main() {
+    let text = "
+        let device_address = 0x0003;
+        let receiver_address = 0x000a;
+
+        send BUTTON_PRESSED_EVENT_CODE from device_address to receiver_address;
+    ";
+
+    println!("{:?}", Parser::parse(text))
+}
