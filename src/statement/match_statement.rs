bd245ceea2fcac30b8ba7c5666d76195b5ca11c9
Linas Nikiperavičius <linas@linasdev.com>
2021-11-28 19:26:39 +0200

    Add producer match extractor

--- src/statement/match_statement.rs
+++ src/statement/match_statement.rs
@@ -5,7 +5,7 @@ use nom::{Err, IResult};
 use std::convert::TryInto;
 
 use ross_config::extractor::Extractor;
-use ross_config::extractor::{EventCodeExtractor, NoneExtractor};
+use ross_config::extractor::{EventCodeExtractor, EventProducerAddressExtractor, NoneExtractor};
 use ross_config::filter::Filter;
 use ross_config::filter::ValueEqualToConstFilter;
 use ross_config::matcher::Matcher;
@@ -13,7 +13,7 @@ use ross_config::Value;
 
 use crate::extractor::extractor;
 use crate::filter::filter;
-use crate::keyword::{event_keyword, match_keyword};
+use crate::keyword::{event_keyword, match_keyword, producer_keyword};
 use crate::literal::literal;
 use crate::parser::{multispace0, multispace1, ParserError};
 use crate::symbol::{close_brace, open_brace, semicolon};
@@ -39,6 +39,32 @@ pub fn match_statement(text: &str) -> IResult<&str, Matcher, ParserError> {
         Ok((input, (extractor, filter))) => return Ok((input, Matcher { extractor, filter })),
         Err(Err::Error(ParserError::ExpectedKeywordFound(_, expected, _)))
             if expected == "event" => {}
+        Err(err) => return Err(Err::convert(err)),
+    }
+
+    let mut producer_match_parser = {
+        let content_parser = map_res(literal, |literal| {
+            let producer_address = literal.try_into()?;
+            let extractor = Box::new(EventProducerAddressExtractor::new()) as Box<dyn Extractor>;
+            let filter = Box::new(ValueEqualToConstFilter::new(Value::U16(producer_address)))
+                as Box<dyn Filter>;
+
+            Ok((extractor, filter))
+        });
+
+        let producer_keyword_parser =
+            preceded(producer_keyword, preceded(multispace1, content_parser));
+        let match_keyword_parser = preceded(
+            match_keyword,
+            preceded(multispace1, producer_keyword_parser),
+        );
+        terminated(match_keyword_parser, semicolon)
+    };
+
+    match producer_match_parser(text) {
+        Ok((input, (extractor, filter))) => return Ok((input, Matcher { extractor, filter })),
+        Err(Err::Error(ParserError::ExpectedKeywordFound(_, expected, _)))
+            if expected == "producer" => {}
         Err(err) => return Err(Err::convert(err)),
     }
 
@@ -150,6 +176,44 @@ mod tests {
     fn event_missing_semicolon_test() {
         assert_eq!(
             match_statement("match  event  0xabab~u16").unwrap_err(),
+            Err::Error(ParserError::ExpectedSymbolFound(
+                "".to_string(),
+                ";".to_string(),
+                "".to_string(),
+            ))
+        );
+    }
+
+    #[test]
+    fn producer_test() {
+        let (input, matcher) = match_statement("match  producer  0xabab~u16;input").unwrap();
+
+        assert_eq!(input, "input");
+        assert_eq!(
+            format!("{:?}", matcher.extractor),
+            format!("{:?}", EventProducerAddressExtractor::new())
+        );
+        assert_eq!(
+            format!("{:?}", matcher.filter),
+            format!("{:?}", ValueEqualToConstFilter::new(Value::U16(0xabab)))
+        );
+    }
+
+    #[test]
+    fn producer_invalid_literal_test() {
+        assert_eq!(
+            match_statement("match  producer  0xabababab~u32;input").unwrap_err(),
+            Err::Error(ParserError::CastFromToNotAllowed(
+                "u32".to_string(),
+                "u16".to_string(),
+            ))
+        );
+    }
+
+    #[test]
+    fn producer_missing_semicolon_test() {
+        assert_eq!(
+            match_statement("match  producer  0xabab~u16").unwrap_err(),
             Err::Error(ParserError::ExpectedSymbolFound(
                 "".to_string(),
                 ";".to_string(),
